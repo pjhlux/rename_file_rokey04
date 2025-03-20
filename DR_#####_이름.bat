35f50ae8bffb7add1f8492af48891de912f27085
pjhlux <62551486+pjhlux@users.noreply.github.com>
2025-03-21 08:45:24 +0900

    Add files via upload

--- DR_#####_이름.bat
+++ DR_#####_이름.bat
@@ -11,21 +11,28 @@ set "TargetDir=%CD%"
 :: PowerShell 실행
 powershell -NoProfile -ExecutionPolicy Bypass -Command ^
     "$path='%TargetDir%';" ^
-    "$OldPattern='과제_4기';" ^
+    "$OldPattern1='과제_4기';" ^
+    "$OldPattern2='파이썬_(\d+)주차_주말과제';" ^
     "$NewNamePrefix='%NewNamePrefix%';" ^
     "[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;" ^
-    "Get-ChildItem -Path $path -File | Where-Object { $_.Name -match $OldPattern } | ForEach-Object {" ^
+    "Get-ChildItem -Path $path -File | ForEach-Object {" ^
     "   $oldName=$_.Name;" ^
-    "   $newName=$oldName -replace [regex]::Escape($OldPattern), $NewNamePrefix;" ^
-    "   $counter=1;" ^
-    "   $baseNewName=$newName;" ^
-    "   $fileExtension=[System.IO.Path]::GetExtension($oldName);" ^
-    "   while (Test-Path -Path (Join-Path -Path $path -ChildPath $newName)) {" ^
-    "       $fileNameOnly=[System.IO.Path]::GetFileNameWithoutExtension($baseNewName);" ^
-    "       $newName=$fileNameOnly + '_' + $counter + $fileExtension;" ^
-    "       $counter++;" ^
+    "   $newName=$oldName;" ^
+    "   if ($oldName -match $OldPattern1) {" ^
+    "       $newName=$oldName -replace [regex]::Escape($OldPattern1), $NewNamePrefix;" ^
+    "   } elseif ($oldName -match $OldPattern2) {" ^
+    "       $newName=$NewNamePrefix + '_파이썬_' + $matches[1] + '주차_주간' + [System.IO.Path]::GetExtension($oldName);" ^
     "   }" ^
-    "   Rename-Item -LiteralPath $_.FullName -NewName $newName -Force;" ^
-    "   Write-Host '변경됨: ' $oldName '→' $newName;" ^
+    "   if ($newName -ne $oldName) {" ^
+    "       $counter=1;" ^
+    "       $baseNewName=$newName;" ^
+    "       $fileExtension=[System.IO.Path]::GetExtension($oldName);" ^
+    "       while (Test-Path -Path (Join-Path -Path $path -ChildPath $newName)) {" ^
+    "           $fileNameOnly=[System.IO.Path]::GetFileNameWithoutExtension($baseNewName);" ^
+    "           $newName=$fileNameOnly + '_' + $counter + $fileExtension;" ^
+    "           $counter++;" ^
+    "       }" ^
+    "       Rename-Item -LiteralPath $_.FullName -NewName $newName -Force;" ^
+    "       Write-Host '변경됨: ' $oldName '→' $newName;" ^
+    "   }" ^
     "}"
-
